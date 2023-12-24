3e69e0b503a6623f5d3d969f5f3e3ae47aa85cf8
gttrcr <gttrcr@gmail.com>
2023-12-24 18:35:24 +0100

    new func for wl

--- start_with_wolfram_jupyter.sh
+++ start_with_wolfram_jupyter.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+if ! command -v wolframscript &> /dev/null
+then #check this branch
+    curl -O -J -L --progress-bar 'https://account.wolfram.com/dl/WolframEngine?version=13.3&platform=Linux&downloadManager=false&includesDocumentation=false'
+    chmod +x WolframEngine_13.3.0_LINUX.sh 
+    sudo ./WolframEngine_13.3.0_LINUX.sh
+    ./WolframKernel
+fi
+
+# xdg-open https://www.wolframcloud.com/users/user-current/activationkeys #list of activations keys
+
+sudo apt install jupyter -y
+rm -rf WolframLanguageForJupyter
+git clone https://github.com/WolframResearch/WolframLanguageForJupyter.git
 cd WolframLanguageForJupyter
 ./configure-jupyter.wls add
+rm -rf WolframLanguageForJupyter
 jupyter kernelspec list
+jupyter notebook
